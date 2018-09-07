6488e3440a8fb328538fc6f8917ba0bd6919d378
sunasooner <31681877+sunasooner@users.noreply.github.com>
2018-09-07 16:31:38 -0400

    second commit

--- hw1-NaSu/app.R
+++ hw1-NaSu/app.R
@@ -8,41 +8,61 @@
 #
 
 library(shiny)
+library(reshape2)
+library(dplyr)
+library(plotly)
+library(shinythemes)
+
+library(shiny)
+library(reshape2)
+library(dplyr)
+library(plotly)
+library(shinythemes)
+
+
+data1 <- subset(mtcars,select = c(1:4))
+name <- row.names.data.frame(mtcars)
+newdata <- data.frame(name,data1)
+newdata$name <- as.factor(newdata$name)
+
+pdf(NULL)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+  navbarPage("Star Wars NavBar", 
+             theme = shinytheme("united"),
+             tabPanel("Plot",
+                      sidebarLayout(
+                        sidebarPanel(
+                          selectInput("char_select",
+                                      "Characters:",
+                                      choices = levels(newdata$name),
+                                      multiple = TRUE,
+                                      selectize = TRUE,
+                                      selected = c("Mazda RX4", "Duster 360", "Honda Civic", "Ford Pantera L", "Merc 450SE"))
+                        ),
+                        # Output plot
+                        mainPanel(
+                          plotlyOutput("plot")
+                        )
+                      )
+             ),
+             # Data Table
+             tabPanel("Table",
+                      fluidPage(DT::dataTableOutput("table"))
+             )
+  )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  output$plot <- renderPlotly({
+    datcars <- subset(newdata, name %in% input$char_select)
+    ggplot(data = datcars, aes(x = name, y = as.numeric(mpg), fill = name)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1))
+  })
+  output$table <- DT::renderDataTable({
+    subset(newdata, name %in% input$char_select, select = c(name, mpg, cyl, disp, hp))
+  })
 }
 
 # Run the application 
